a983a68e7784f91a7fa6998d93ddb3581e713905
Francisco Esquibel <f.esquibelalvarez@gmail.com>
2019-12-09 20:01:11 -0300

    Declaring interfaces

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
 #include <iostream>
+#include <string>
+
+class Observer {
+    virtual void Update(std::string data) = 0;
+};
+
+class Subject {
+public:
+    virtual void RegisterObserver(Observer* observer) = 0;
+    virtual void RemoveObserver(Observer* observer) = 0;
+    virtual void NotifyObserver() = 0;
+};
+
+class Publisher : public Subject {
+private:
+
+};
 
 int main(int argc, char* argv[]) {
     std::cout << "Publish-Subcribe Pattern" << std::endl;
